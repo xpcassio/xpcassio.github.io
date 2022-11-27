25eecd512fbc0149db3b63fb5ddf90c218807428
Cássio Nascimento <xpcassio@MacBook-Air-de-Cassio.local>
2022-11-27 18:15:03 -0300

    update deploy workflow fix pt4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://xpcassio.github.io',
-    base: '/xpcassio.github.io'
+    site: 'https://xpcassio.github.io'
 });
