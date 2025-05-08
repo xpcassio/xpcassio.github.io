0b41bb7e269538a34ea7569b8529fed76484dfbd
Cássio Nascimento <xpcassio@gmail.com>
2025-05-08 18:48:12 -0300

    Update astro e organizando projeto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://xpcassio.github.io'
+    site: 'https://xpcassio.github.io',
+    i18n: {
+        locales: ["pt-br", "en"],
+        defaultLocale: "pt-br",
+    }
 });
