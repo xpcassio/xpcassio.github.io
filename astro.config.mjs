ddeb4117956699f5aaf501d9ae27370c3b813058
Cássio Nascimento <xpcassio@gmail.com>
2025-05-09 08:23:36 -0300

    Arrumando internacionalização e componente header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://xpcassio.github.io',
     i18n: {
-        locales: ["pt-br", "en"],
+        locales: ["pt-br", "en", "es", "pt"],
         defaultLocale: "pt-br",
+        fallback: {
+            pt: "pt-br",
+            es: "en"
+        },
+        routing: {
+            prefixDefaultLocale: true,
+            redirectToDefaultLocale: true,
+            fallbackType: "redirect",
+        }
     }
 });
