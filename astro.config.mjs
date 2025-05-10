483cc4aea8eff94988251297b99e180827dd3d36
Cássio Nascimento <xpcassio@gmail.com>
2025-05-10 09:10:47 -0300

    Arrumando menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://xpcassio.github.io',
+    trailingSlash: "never",
     i18n: {
         locales: ["pt-br", "en", "es", "pt"],
         defaultLocale: "pt-br",
